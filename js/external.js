b33d3894fc3932cf793c83dbe44735a0963dab25
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-01-20 14:18:19 -0600

    javascript with html

---
+++ js/external.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("hi its zy bored loving school");
+
+
+//Alert- How do I first open up a window to show my user some information?
+alert(" what will appear in alert window");
+
+//Can also use variables
+var myName = "Zy"
+ alert( " hey this is " + myName + "talking to you");
+
+
+//Can it run an expression?
+
+alert(2+2);
+
+alert(true && false);
+
+
+var likedlunch = confirm("how was the food");
+
+alert("you told me that it was") + likedlunch + " that you likedlunch"
