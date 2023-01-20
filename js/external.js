9fca7487c9464e2be37762afe9bce8e526b0104e
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-01-20 14:29:16 -0600

    js with html

--- js/external.js
+++ js/external.js
@@ -3,7 +3,7 @@
 console.log("hi its zy bored loving school");
 
 
-//Alert- How do I first open up a window to show my user some information?
+//Alert -How do I first open up a window to show my user some information?
 alert(" what will appear in alert window");
 
 //Can also use variables
