0a56b0b0da581724c284d3bd98ffc264317f4b51
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-01-30 11:40:23 -0600

    'almost finished with excersise'

---
+++ js/for_loops.js
@@ -0,0 +1,21 @@
+function showMultiplicationTable(number) {
+    for(var i = 1; i <= 10; i++  ){
+        console.log("the count is " + (number * i)  );
+    }
+
+}
+//first part is where we are starting , 2nd part is where you want it to stop/checks to see if true or false
+//3rd part add on every time
+for(var i = 1; i <= 10; i++ ) {
+    var random = Math.floor((Math.random() * 180) + 20);
+    if (random % 2 === 0) {
+        console.log("its even " + random);
+    } else {
+        console.log("its odd " + random);
+    }
+}
+
+for(var i = 1; i <= 10; i++ ) {
+    let num = 2string . (i)repeat
+
+}
